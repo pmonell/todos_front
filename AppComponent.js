7ccfc8c0226955e1ab06adce9dd2ce9bc9d0b639
Phillip Monell <pmonell@gmail.com>
2017-06-07 16:02:34 -0400

    Adds react component template

---
+++ AppComponent.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class AppComponent extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+
+  }
+}
