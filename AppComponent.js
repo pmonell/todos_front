1090a38faf3608fb7c4ccac2fb96026249c17e22
Phillip Monell <pmonell@gmail.com>
2017-06-07 18:22:36 -0400

    Fetch some todos

--- AppComponent.js
+++ AppComponent.js
@@ -4,17 +4,34 @@ import ReactDOM from 'react-dom'
 class AppComponent extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      // set initial todos to null
+      todos: []
+    }
   }
 
   componentDidMount() {
-
+    fetch("http://192.168.99.102:3000/todos", {
+      method: "GET",
+      headers: new Headers({
+        'content-type': 'application/json; charset=utf-8'
+      })
+    }).then(response => {
+      return response.json()
+    }).then(json => {
+      this.setState({todos: json.todos})
+    })
   }
 
   render() {
+    const { todos } = this.state
     return (
       <div>
-        Hello
+        <div>
+          {todos.map((todo, i) =>
+            <p key={i}>{todo.text}</p>
+          )}
+        </div>
       </div>
     )
   }
